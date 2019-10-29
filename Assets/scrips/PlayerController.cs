b1d1883e0a172ba43057e626760be5f2ec41c231
raguiarperez <raguiarperez@danielcastelao.org>
2019-10-29 10:42:46 +0100

    Roll a Ball

---
+++ Assets/scrips/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour {
+
+private Rigidbody rb;
+private int count;
+public float speed;
+public Text countText;
+public Text winText;
+	void SetCountText(){
+		countText.text="Score : " + count.ToString();
+		if (count >= 8) {
+ 		winText.text = "You Win";
+		 
+}
+	}
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		count=0;
+		SetCountText();
+		winText.text = "";
+
+	}
+	
+	void Update () {
+		 float moveHorizontal = Input.GetAxis("Horizontal");
+ 		float moveVertical = Input.GetAxis("Vertical");
+ 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+ 		rb.AddForce(movement * speed);
+	}
+	void OnTriggerEnter(Collider other) {
+ 		if (other.gameObject.tag == "PickUp") {
+ 		other.gameObject.SetActive(false);
+ 		count = count +1;
+ 		SetCountText(); 
+ }
+
+}
+}
---
+++ Assets/scrips/SampleAgentScript.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SampleAgentScript : MonoBehaviour {
+
+public Transform target;
+private NavMeshAgent agent;
+
+public Animator animator;
+
+public float proximidad;
+
+
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.autoBraking = false;
+		animator = target.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination(target.position);
+
+		if (!agent.pathPending && agent.remainingDistance < proximidad){
+            Debug.Log("Peligro");
+            // cambiamos a true la variable del animator
+            animator.SetBool("EstarEnPeligro", true);
+		}
+        else{
+            Debug.Log("Tranqui");
+            // cambiamos a false la variable del animator
+            animator.SetBool("EstarEnPeligro", false);
+        }
+	}
+}
